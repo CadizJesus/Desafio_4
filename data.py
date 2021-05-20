58235f22e2218d7af1d5dab3b6e5fde894175b57
ChechoLatorre <sergio.latorrecaballero.96@gmail.com>
2021-05-20 19:16:02 -0400

    bad

--- data.py
+++ data.py
@@ -1,4 +1,26 @@
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def load_data():
-    return 1
+class Data:
+    X_train = None
+    y_train = None
+    X_test = None
+    y_test = None
+
+    def __init__(self):
+        (self.X_train, self.y_train),(self.X_test, self.y_test) = self.load_data()
+
+
+    def load_data(self):
+        df = pd.read_csv('data.csv')
+
+        #se dropean columnas que consideramos que no nos sirven
+        df = df.drop(columns=['Unnamed: 0','song_title','artist'])
+
+        X = df[list(df.columns[:-1])]
+        y = df[[df.columns[-1]]]
+
+        (X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.3, random_state=42)
+        
+        return (X_train, y_train),(X_test, y_test)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from data import load_data
+from data import Data
 
 class Neuron:
     previous_layer = []
@@ -7,35 +7,63 @@ class Neuron:
     value = float()
     weight_updated = []
     next_layer = []
-
+    
     def __init__(self, previous_layer = None, previus_weights = None, value = None):
         self.previous_layer = previous_layer
-        self.previus_weights = previus_weights
-        self.value = value
+        self.previus_weights = previous_layer
+        if not value:
+            self.value = value
+        else:
+            self.value = self.calculate_value()
 
     def act_sigmoid(self):
         return 1/(1+np.exp(-self.value))
 
     def derivate_sigmoid(self,a):
-        return a*(1-a)
+            return a*(1-a)
 
     def calculate_value(self):
         value = 0
         for i in range(len(self.previous_layer)):
-            value += self.previous_layer[i].act_sigmoid() * self.previous_weights[i]
+             value += self.previous_layer[i].act_sigmoid() * self.previous_weights[i]
         return value
-
 
 class Network:
     list_layers = []
-    data        = None
-       
+    data = None
+
     def __init__(self):
         self.list_layers = []
+        data = Data()
 
-    def add_layer(self):
+    def add_layer(self, n_neuronas, row):
+        layer = []
+        if not self.list_layers:
+            layer = self.add_first_layer(row)
+        if len(self.list_layers) == 1:
+            for n in range(n_neuronas):
+                layer.append(Neuron(previus_weights = self.ini_weight(self.list_layers[0]), previous_layer = self.list_layers[0]))
+        else:
+            for n in range(n_neuronas):
+                layer.append(Neuron(previus_weights = self.ini_weight(self.list_layers[-1]), previous_layer = self.list_layers[-1]))
 
-        return 1
+        self.list_layer.append(layer)
 
-    def load_data(self):
-        self.data
+    def add_first_layer(self, row):
+        layer = []
+        for i in range(len(row)):    
+            layer.append(Neuron(value = row[i]))
+        return layer
+    
+    def ini_weight(self, first_layer):
+        random_weights = []
+        
+        for i in range(len(first_layer)):
+            weight = np.random.random_sample()   
+            random_weights.append(weight)
+
+        return random_weights
+
+
+a = Network()
+a.add_layer(5,[1,2,3,4,5])
